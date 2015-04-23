3b7ebbdf4ddc971d780be51e663c0d0dbe801cfe
Max White <mushishi78@gmail.com>
2015-04-23 13:35:17 +0100

    Move Yotpo to product page only

--- assets/_js/pages/product.js
+++ assets/_js/pages/product.js
@@ -4,4 +4,7 @@ if(document.getElementById('product-page')) {
 	JSE.Stores.Basket.listen(function() {
 		window.location.href = '{{ site.baseurl }}/basket';
 	});
+
+	// Load Yotpo
+  (function e(){var e=document.createElement("script");e.type="text/javascript",e.async=true,e.src="//staticw2.yotpo.com/0aHEMLDicyiDM2Fb1ZTDuCMMharSoZrmwbq2LFhG/widget.js";var t=document.getElementsByTagName("script")[0];t.parentNode.insertBefore(e,t)})();
 }
